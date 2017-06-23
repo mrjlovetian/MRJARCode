4304c8901999c104905e60e9f9cca20de040d3b0
余洪江 <kakao@xiaoguandeMacBook-Pro.local>
2017-06-23 12:36:44 +0800

    文件目录修改

--- YHJQRCode/Classes/NSBundle+YHJQRCode.m
+++ YHJQRCode/Classes/NSBundle+YHJQRCode.m
@@ -17,24 +17,23 @@
 
 + (NSString *)YHJQRCodeLocalizedStringForKey:(NSString *)key value:(NSString *)value
 {
-    static NSBundle *bundle = nil;
-    if (bundle == nil) {
-        // （iOS获取的语言字符串比较不稳定）目前框架只处理en、zh-Hans、zh-Hant三种情况，其他按照系统默认处理
-        NSString *language = [NSLocale preferredLanguages].firstObject;
-        if ([language hasPrefix:@"en"]) {
-            language = @"en";
-        } else if ([language hasPrefix:@"zh"]) {
-            if ([language rangeOfString:@"Hans"].location != NSNotFound) {
-                language = @"zh-Hans"; // 简体中文
-            } else { // zh-Hant\zh-HK\zh-TW
-                language = @"zh-Hant"; // 繁體中文
-            }
-        } else {
-            language = @"en";
+    NSBundle *bundle = nil;
+    // （iOS获取的语言字符串比较不稳定）目前框架只处理en、zh-Hans、zh-Hant三种情况，其他按照系统默认处理
+    NSString *language = [NSLocale preferredLanguages].firstObject;
+    if ([language hasPrefix:@"en"]) {
+        language = @"en";
+    } else if ([language hasPrefix:@"zh"]) {
+        if ([language rangeOfString:@"Hans"].location != NSNotFound) {
+            language = @"zh-Hans"; // 简体中文
+        } else { // zh-Hant\zh-HK\zh-TW
+            language = @"zh-Hant"; // 繁體中文
         }
-        
-        bundle = [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:language ofType:@"lproj" inDirectory:@"YHJQRCode.bundle"]];
+    } else {
+        language = @"en";
     }
+    
+    bundle = [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:language ofType:@"lproj" inDirectory:@"YHJQRCode.bundle"]];
+    
     value = [bundle localizedStringForKey:key value:value table:nil];
     return [[NSBundle mainBundle] localizedStringForKey:key value:value table:nil];
 }
